9ec4967b1af00522a3ef68dd76011e8aadc06e71
Jay Carlson <jaycarlson@exosite.com>
2018-10-04 16:39:19 -0500

    updates to readme, some comments

--- insights/examples/murano/modules/insightModule.lua
+++ insights/examples/murano/modules/insightModule.lua
@@ -4,8 +4,11 @@ function insightModule.addNumbers(body)
   local dataIN = body.data
   local constants = body.args.constants
   dataOUT = {}
+
+-- dataIN is a list of datapoints
   for _, dp in pairs(dataIN) do
 
+    -- Each signal value in dataOUT should keep the incoming metadata
     dp.value = dp.value + constants.adder
 
     table.insert(dataOUT, dp)
